52378699353bae66771dd0c696945874bf9e1e7f
rabiul97 <rabiulhasanchy1997@gmail.com>
2022-01-22 21:36:45 +0600

    Flying-Shooter

---
+++ app/src/androidTest/java/com/heyletscode/ihavetofly/AndroidJUnit4.java
@@ -0,0 +1,17 @@
+package com.heyletscode.ihavetofly;
+
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+
+public class AndroidJUnit4 extends Runner {
+    @Override
+    public Description getDescription() {
+        return null;
+    }
+
+    @Override
+    public void run(RunNotifier runNotifier) {
+
+    }
+}
---
+++ app/src/androidTest/java/com/heyletscode/ihavetofly/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.heyletscode.ihavetofly;
+
+import static junit.framework.TestCase.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.heyletscode.ihavetofly",
+                appContext.getPackageName());
+    }
+}
